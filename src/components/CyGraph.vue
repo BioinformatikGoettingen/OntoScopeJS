340983f43c28ce132fde17a7bbaf422b03fbc1d5
Jürgen Dönitz <jdo@dragonflydock.bioinf.med.uni-goettingen.de>
2019-02-25 17:16:33 +0100

    Data is queried from OBA service and added to the graph.

--- src/components/CyGraph.vue
+++ src/components/CyGraph.vue
@@ -6,10 +6,39 @@
                 :afterCreated="afterCreated"
                 :debug="true"
         />
+        <input v-model="searchTerm" v-on:keyup.enter="search_for_class(searchTerm)"></input>
+        <button v-on:click="search_for_class(searchTerm)">Search</button>
+
     </div>
 </template>
 
 <script>
+    import axios from 'axios'
+
+    class Ontology_class {
+
+        constructor(json) {
+            this.json = json;
+        }
+
+        get id() {
+            return this.json.name;
+        }
+
+        get label() {
+            // later on switch between name of the class or label annotation
+            // also switch language
+            //
+            for (var anno of this.json.annotations) {
+                if (anno.name === 'label') {
+                    //this.label = anno.value;
+                    return anno.value;
+                }
+            }
+            return this.json.name;
+        }
+    }
+
     /* eslint-disable */
     const config = {
         style: [
@@ -31,11 +60,43 @@
             }
         ]
     };
+
+    function add_first(json_response, cy) {
+        //temporary function to add the first node, will be replaced by a selection list
+        if (json_response.status !== 200 || json_response.data.entities.length < 1) {
+            console.log("no entities found")
+            return;
+        }
+        console.log(cy)
+        var onto_cls = new Ontology_class(json_response.data.entities[0]);
+        //console.log(onto_cls);
+        add_node_with_parents(onto_cls, cy);
+    }
+
+    function add_node_with_parents(onto_cls, cyInst) {
+        //todo check if node is already in graph.
+        cyInst.then(cy => {
+            cy.add([{group: 'nodes', data: {id: onto_cls.id, label: onto_cls.label}}]);
+            for (const p of onto_cls.json.parents) {
+
+                var parent_cls = new Ontology_class(p); // set flag to fill object
+                cy.add([{group: 'nodes', data: {id: parent_cls.id, label: parent_cls.label, json: parent_cls}},
+                    {group: 'edges', data: {source: parent_cls.id, target: onto_cls.id}}]);
+            }
+            cy.layout({
+                name: 'cose'
+            }).run();
+        })
+
+
+    }
+
     export default {
         name: "CyGraph",
         data() {
             return {
-                config
+                config,
+                searchTerm: ""
             };
         },
         methods: {
@@ -50,20 +111,20 @@
             setCyElements(cy) {
                 console.log("setCyElements");
                 let cytoElems = [
-                    {
-                        // node a
-                        data: { id: "a" },
-                        position: { x: 100, y: 100 }
-                    },
-                    {
-                        // node b
-                        data: { id: "b" },
-                        position: { x: 200, y: 100 }
-                    },
-                    {
-                        // edge ab
-                        data: { id: "ab", source: "a", target: "b" }
-                    }
+                    // {
+                    //     // node a
+                    //     data: {id: "a"},
+                    //     position: {x: 100, y: 100}
+                    // },
+                    // {
+                    //     // node b
+                    //     data: {id: "b"},
+                    //     position: {x: 200, y: 100}
+                    // },
+                    // {
+                    //     // edge ab
+                    //     data: {id: "ab", source: "a", target: "b"}
+                    // }
                 ];
                 cy.startBatch();
                 cy.elements().remove();
@@ -72,9 +133,24 @@
                 }
                 cy.endBatch();
                 cy.fit();
-            }
+            },
+            search_for_class(searchString, url = "http://oba.sybig.de", ontology = "tribolium") {
+                axios.defaults.headers = {
+                    'Accept': 'application/json'
+                }
+                var cy = this.$cytoscape.instance
+                axios.get(url + '/' + ontology + "/functions/basic/searchCls/" + searchString)
+                    .then(function (response) {
+
+                        add_first(response, cy);
+                    });
+            },
+
+
         }
     };
+
+
 </script>
 
 <style>
@@ -83,12 +159,13 @@
         height: 600px;
         border: 1px red solid;
     }
+
     /*#app {*/
-        /*font-family: "Avenir", Helvetica, Arial, sans-serif;*/
-        /*-webkit-font-smoothing: antialiased;*/
-        /*-moz-osx-font-smoothing: grayscale;*/
-        /*text-align: left;*/
-        /*color: #2c3e50;*/
-        /*margin-top: 60px;*/
+    /*font-family: "Avenir", Helvetica, Arial, sans-serif;*/
+    /*-webkit-font-smoothing: antialiased;*/
+    /*-moz-osx-font-smoothing: grayscale;*/
+    /*text-align: left;*/
+    /*color: #2c3e50;*/
+    /*margin-top: 60px;*/
     /*}*/
 </style>
