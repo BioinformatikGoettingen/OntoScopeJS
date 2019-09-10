935f1f9476680cf226b46d81fe7cdd9d6f05e8b7
malte voskamp <maltevoskamp@maltes-MacBook-Pro-2.local>
2019-09-10 17:25:06 +0200

    added vue js component to create the infopanel

---
+++ src/components/Results.vue
@@ -0,0 +1,30 @@
+// Results.vue file
+<template>
+  <div style = "border-style: solid; border-width:2px; float:right;">
+    <li v-for="(message, index) in messageList" :item="message" :key="index">
+      {{ message }}
+    </li>
+    <h3> Name : {{ name }} </h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Results",
+  props: {
+    msg: {
+      type: String
+    }
+  },
+  data: function() {
+    return {
+      messageList: []
+    };
+  },
+  watch: {
+    msg: function() {
+      this.messageList.push(this.msg);
+    }
+  }
+};
+</script>
