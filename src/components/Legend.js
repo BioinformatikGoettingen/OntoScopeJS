c0c65b24dd758f5116f6558b2da3ad8969ee2938
malte voskamp <maltevoskamp@maltes-MBP-2.fritz.box>
2020-05-29 10:49:28 +0200

    overworked Legend.js

---
+++ src/components/Legend.js
@@ -0,0 +1,104 @@
+export default class Legend {
+    constructor() {
+        //legendlist include nodes and edges
+        //evtl laden der farben aus der config
+        this.nodelist = {}
+        this.edgelist = {}
+    }
+
+    get list() {
+        return this.nodelist
+    }
+    
+    set list(json) {
+        this.nodelist = json
+        return this.nodelist
+    }
+
+    add_node_to_legend(node_cat,color){
+        if(!this.nodelist[node_cat]){
+            this.nodelist[node_cat] = color
+            this.update_dom()
+        }
+        return this.nodelist
+    }
+
+    add_edge_to_legend(edge_cat,color){
+        if(!this.edgelist[edge_cat]){
+            this.edgelist[edge_cat] = color
+            this.update_dom()
+        }
+        return this.edgelist
+    }
+
+    update_dom() {
+        var nodetable = document.getElementById("nodetable")
+        var nodelist = this.nodelist        
+
+        Object.keys(this.nodelist).forEach(function(key) {
+            console.log(key + nodelist[key])
+            if(!document.getElementById("nodelegend:" + key)) {
+                var tablerow = document.createElement("tr")
+                var dot = document.createElement("td")
+                var nodedesc = document.createElement("td")
+
+                nodedesc.id = "nodelegend:" + key 
+
+            
+                var dotnode = document.createElement("span")
+                dotnode.className += "dot"
+                dotnode.style.backgroundColor = nodelist[key]
+
+                var descnode = document.createTextNode(key)
+
+                dot.appendChild(dotnode);
+                nodedesc.appendChild(descnode);
+
+                tablerow.appendChild(dot)
+                tablerow.appendChild(nodedesc)
+
+                nodetable.appendChild(tablerow)
+            }
+        })
+
+        var edgetable = document.getElementById("edgetable")
+        var edgelist = this.edgelist
+
+        Object.keys(this.edgelist).forEach(function(key) {
+            console.log(key + edgelist[key])
+            if(!document.getElementById("edgelegend:" + key)) {
+                var tablerow = document.createElement("tr")
+                var dot = document.createElement("td")
+                var edgedesc = document.createElement("td")
+
+                edgedesc.id = "edgelegend:" + key
+
+            
+                var dotnode = document.createElement("span")
+                dotnode.className += "vl"
+                dotnode.style.borderColor = edgelist[key]
+
+                var descnode = document.createTextNode(key)
+
+                dot.appendChild(dotnode);
+                edgedesc.appendChild(descnode);
+
+                tablerow.appendChild(dot)
+                tablerow.appendChild(edgedesc)
+
+                edgetable.appendChild(tablerow)
+                
+            }
+        })
+
+       /* var tablerow = document.createElement("tr")
+        var dot = document.createElement("td")
+        var dotname = document.createElement("td")
+        
+        var dotnode = document.createElement("span")
+        dotnode.addclassName += "dot"
+        dotnode.style.backgroundColor = colors[0]*/
+
+
+    }
+}
