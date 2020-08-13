8981dfba0ce1352bbb55eeb55a9db1c9fe9fbb8f
malte voskamp <maltevoskamp@maltes-MacBook-Pro-2.local>
2020-08-13 15:27:04 +0200

    added generic Connector file

---
+++ src/components/genericConnector.js
@@ -0,0 +1,113 @@
+import axios from 'axios'
+import OntoCls from "./OntoCls";
+class genericConnector {
+
+    constructor() {
+      var urlParams = new URLSearchParams(window.location.search);
+      this.url = urlParams.get("ontology")
+      this.ontoname = urlParams.get("ontologyname")
+      //this. url = "http://oba.sybig.de/"
+      //this.ontoname = "cytomer" 
+      //this.category = "Development Stage";
+      this.edgecolorpool =["#f98d06","#fdcd04","#f0ff00","f6c398","#DAF7A6","#581845"]  
+      this.nodecolorpool = ["#001f3f","#0074D9","#7FDBFF","#39CCCC","#3D9970","#2ECC40","#01FF70","#FFDC00","#FF851B","#FF4136","#85144b","#F012BE","#B10DC9","#111111","#AAAAAA","#DDDDDD"]
+      this.edge_color =[]
+      this.node_color=[]
+    }
+
+    async get_node_color(onto_cls){
+      
+      return this.nodecolorpool[0]
+
+    }
+
+
+    //ordnet jeder edge art nach und nach eine Farbe zu
+    get_edge_color(edge_type) {
+      if(edge_type in this.edge_color) {
+        return this.edge_color[edge_type]
+      }else {
+        this.edge_color[edge_type] = this.edgecolorpool[0]
+        this.edgecolorpool.shift()
+        return this.edge_color[edge_type]
+      }
+      
+    }
+    async search_for_class(searchString) {
+      axios.defaults.headers = {
+          'Accept': 'application/json'
+      };
+
+      const response = await axios.get(this.url + "/" + this.ontoname + "/functions/basic/searchCls/" + searchString + "*")
+      //TODO if their is no result, repeat the search once with wildcard
+      let result_cls = []
+      var data = response.data.entities[0]
+      for (let jsonCls of response.data.entities) {
+          let cls = this.createNewOntoCs(jsonCls)
+          cls.fillWithTemplate(data)
+          result_cls.push(cls)
+      }
+      return result_cls
+  }
+  //returns the results in an array
+  async first_search(searchString) {
+      axios.defaults.headers = {
+          'Accept': 'application/json'
+      };
+      const response = await axios.get(this.url + "/" +this.ontoname +"/functions/basic/searchCls/" + searchString + "*")
+      let result_cls = []
+
+      for (let jsonCls of response.data.entities) {
+        var data = jsonCls;
+          let cls = this.createNewOntoCs(jsonCls)
+          //fill with template lite, das nur das label und den namen und die annotations übernimt, aber keine e
+          // eigene Anfrage an den Server stellt, somit sollte die Performance deutlich verbessert werden
+
+
+          //cls.fillCls()
+          result_cls.push(cls)
+      }
+      console.log(result_cls)
+      return result_cls
+  }
+
+  async get_cls_data(Cls) {
+      try {
+          var cls_id = Cls.id;
+          var namespace = Cls.oc_namespace
+          console.log("fetching " + cls_id)
+          console.log("namespace")
+          console.log(namespace)
+          axios.defaults.headers = {
+              'Accept': 'application/json'
+          };
+          console.log(this.url + "/"+this.ontoname+"/cls/" + cls_id + "?ns=" + namespace)
+          let response = await axios({
+              url: this.url + "/"+this.ontoname+"/cls/" + cls_id + "?ns=" + namespace, // add also (encoded) NS
+  
+              method: "get",
+              timeout: 8000
+          });
+
+          console.log("response")
+          console.log(response)
+          return await response
+
+      } catch (err) {
+          console.log(err)
+      }
+  }
+  
+  async getColorCatOfCls(Cls) {
+    return undefined
+  }
+
+  createNewOntoCs(json) {
+      let cls = new OntoCls(json, this)
+      return cls
+  }
+}
+
+
+
+export {genericConnector} 
